06b63f1f8a0e523e38983bb2489bff9dfd637fb9
Milkdromeda Studios <Studio.Milkdromeda@planetmail.net>
2026-05-24 08:57:28 -0700

    Remake posts route fix using single pages/posts.js entry

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
-// digitbox/pages/_app.js
 import "../styles/global.css";
 import "../styles/login.css";
 import Layout from "../components/Layout";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, router }) {
+  if (router?.pathname?.startsWith("/projects/")) {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
