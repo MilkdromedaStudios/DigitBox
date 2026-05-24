b9ced7a9af93ead972d94cbf481b5a6769e8cca3
Milkdromeda Studios <Studio.Milkdromeda@planetmail.net>
2026-05-24 09:43:04 -0700

    Add customizable profile preferences with cookie persistence

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
 import "../styles/global.css";
 import "../styles/login.css";
 import Layout from "../components/Layout";
+import { readProfilePrefsFromCookie } from "../lib/profilePreferences";
 
 export default function MyApp({ Component, pageProps, router }) {
+  useEffect(() => {
+    const applyTheme = () => {
+      const prefs = readProfilePrefsFromCookie();
+      document.body.dataset.theme = prefs.theme || "dark";
+      document.body.style.setProperty("--accent", prefs.accentColor || "#8b5cf6");
+    };
+
+    applyTheme();
+    window.addEventListener("focus", applyTheme);
+    return () => window.removeEventListener("focus", applyTheme);
+  }, []);
+
   if (router?.pathname?.startsWith("/projects/")) {
     return <Component {...pageProps} />;
   }
