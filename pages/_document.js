a860595933dac8bee28e9761952d8f220ea68854
Milkdromeda Studios <Studio.Milkdromeda@planetmail.net>
2026-08-10 15:00:11 -0700

    Remove AdSense script from main DigitBox site

--- pages/_document.js
+++ pages/_document.js
@@ -7,11 +7,6 @@ export default function Document() {
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="apple-touch-icon" href="/favicon.svg" />
         <meta name="theme-color" content="#0b1330" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2739531636775993"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
       <body>
         <Main />
